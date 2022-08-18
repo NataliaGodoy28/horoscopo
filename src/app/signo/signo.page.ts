8bceece0aa222f638d370cb164c015a2f26067b2
natalia <na.godoy@duocuc.cl>
2022-08-18 17:14:56 -0400

    segundo repo

--- src/app/signo/signo.page.ts
+++ src/app/signo/signo.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -9,12 +10,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class SignoPage implements OnInit {
 
   signo: string;
+  data: any
 
-  constructor(private router:Router, private active: ActivatedRoute) {
+  constructor(private router:Router, private active: ActivatedRoute,private http: HttpClient) {
     this.active.queryParams.subscribe(params =>{
       this.signo = this.router.getCurrentNavigation().extras.state.signo;
-      console.log(this.signo);
+      this.getHoroscopo("https://aztro.sameerkumar.website/?sign="+this.signo+"&day=today")
     });
+   }
+
+   getHoroscopo(url:string){
+      this.http.post(url,"").subscribe(data => {
+        this.data = data
+        console.log(this.data);
+      })
    }
 
   ngOnInit() {
